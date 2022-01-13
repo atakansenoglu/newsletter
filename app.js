db3fd578a7356643e373604491ca41f840f66c75
Atakan Şenoğlu <33118404+atakansenoglu@users.noreply.github.com>
2022-01-13 16:30:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const https = require('https')
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post('/', (req, res) => {
+    
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+      members: [
+        {
+          email_address: email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName,
+          }
+        }
+      ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us20.api.mailchimp.com/3.0/lists/8547fe47bb"
+
+    const options = {
+      method: "POST",
+      auth: "atakan:343751b5f2aaf3b5186c38d2bb723ee9-us20"
+    }
+
+    const request = https.request(url, options, function(response) {
+
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html")
+      } else {
+        res.sendFile(__dirname + "/failure.html")
+      }      
+
+      response.on("data", (data) => {
+        console.log(JSON.parse(data));
+      })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", (req, res) => {
+  res.redirect("/")
+})
+
+app.listen(port || 3000, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+// API Key
+// 343751b5f2aaf3b5186c38d2bb723ee9-us20
+
+// Audience ID
+// 8547fe47bb
